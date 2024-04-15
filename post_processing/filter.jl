6cf104169185bce93f4900a27ae9c244c2d4c08c
Wren <146726895+wrenstuart@users.noreply.github.com>
2024-04-15 19:51:25 +0100

    figured out how to do a low-pass filter

---
+++ post_processing/filter.jl
@@ -0,0 +1,32 @@
+using CairoMakie
+using FFTW
+
+f(x, y) = cos(x + 2y) - 3sin(5x) + 2cos(10y)
+Δ = 0.1
+x = [x for x in -5π:Δ:5π]
+y = [y for y in 0:Δ:6π]
+M = length(x)
+N = length(y)
+z = [f(x, y) for x in x, y in y]
+
+L = 1   # Filter lengthscale
+
+# Outline of how wavenumbers work is below
+#=
+k_min = 2π/(x[end] - x[1])
+l_min = 2π/(y[end] - y[1])
+k = [k_min * (m-1) for m in 1:M]
+l = [l_min * (n-1) for n in 1:N]
+# Last values of above arrays should be 2π/Δ ^
+k_cut = 2π/L
+=#
+
+m_cut = Int(round((x[end] - x[1])/L))
+n_cut = Int(round((y[end] - y[1])/L))
+
+z_f = fft(z)
+z_f[1+m_cut:M-m_cut, :] .= 0
+z_f[:, 1+n_cut:N-n_cut] .= 0
+z_smooth = real(ifft(z_f))
+
+heatmap(x, y, z_smooth)
