a167f8dc6ced36d78d2a6668a233fc0871c8d50b
Wren Stuart <mathswren@gmail.com>
2025-10-16 14:29:28 +0100

    Some sims done

---
+++ simulation/inputs/visc2-test.jl
@@ -0,0 +1,12 @@
+function sim_params() 
+    Ri = 1
+    s = 1e4
+    ν_v = 2e-3
+    ν_h = 8e+1
+    GPU = true
+    res = (512, 512, 64)
+    advection_scheme = CenteredSecondOrder
+    horizontal_hyperviscosity = false
+    short_duration = false
+    return (; GPU, res, Ri, s, ν_h, ν_v, advection_scheme, horizontal_hyperviscosity, short_duration)
+end
